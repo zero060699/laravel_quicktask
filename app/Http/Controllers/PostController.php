4eb914a591602497213f0b54e77d7749ce3f058f
zero06061999 <builoc191999@gmail.com>
2020-12-22 10:03:58 +0700

    migration and view

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('index')->with('posts', $posts);
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layout')
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-center">{{ trans('lable.crud') }}</h1>
+        </div>
+    </div>
+    <div class="col-md-12 text-right">
+        <a href="{{ route('posts.create') }}" class="btn btn-primary">{{ trans('label.add') }}</a>
+    </div>
+    <table class="table table-bordered">
+        <thead>
+            <tr class="text-center">
+                <th>{{ trans('label.no') }}</th>
+                <th>{{ trans('lable.name') }}</th>
+                <th>{{ trans('lable.detail') }}</th>
+                <th>{{ trans('lable.author') }}</th>
+                <th>{{ trans('lable.action') }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php
+               $count = 1;
+            @endphp
+            @foreach ($posts as $post)
+            <tr class="text-center">
+                <td>{{ $count++ }}</td>
+                <td>{{ $post->name }}</td>
+                <td>{{ $post->detail }}</td>
+                <td>{{ $post->author }}</td>
+                <td>
+                    <form action="{{ route('posts.destroy', $post->id) }}">
+                        <a href="#" class="btn btn-info">{{ trans('lable.comment') }}</a>
+                        <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-warning">{{ trans('lable.edit') }}</a>
+                        <button type="submit" class="btn btn-danger">{{ trans('label.delete') }}</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
